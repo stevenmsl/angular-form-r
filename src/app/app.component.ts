3cbd5efc5a5f61780dca3591498f687595e4fedc
Steven Lin <steven_lx@outlook.com>
2020-07-03 15:34:49 -0700

    initial check-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-form-r';
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUserNames = ['Admin', 'Sa'];
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      // you pass the reference of the validator method
+      // to Angular; you don’t call them right here. Angular
+      // will call them at the time needed.
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          // you need to access other property defined on this class,
+          // and hence you need to bind ‘this’ as well
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    // this.signupForm.statusChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    this.signupForm.setValue({
+      userData: {
+        username: 'Arlo',
+        email: 'arlo@arlo.com',
+      },
+      gender: 'male',
+      hobbies: [],
+    });
+
+    this.signupForm.patchValue({
+      userData: {
+        username: 'Arlo2',
+        email: 'arlo@arlo.com',
+      },
+    });
+  }
+  onSubmit() {
+    console.log(this.signupForm);
+    this.signupForm.reset();
+  }
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null; // If succeeded return null
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
